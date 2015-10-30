adcafbc33c2f51469f41810f934ad5ffeca9ef8a
1223558098 <ios-15@ACC8084B.ipt.aol.com>
2015-10-30 11:16:06 +0800

    12
    广大官兵地方办法

---
+++ gfd/gfd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gfd
+//
+//  Created by ios－15 on 15/10/30.
+//  Copyright © 2015年 xiaov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gfd/gfd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gfd
+//
+//  Created by ios－15 on 15/10/30.
+//  Copyright © 2015年 xiaov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
